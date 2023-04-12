44129ecc9afde0e703334937c283168cbdc3dcb1
Dike-Code <dikeclintonobinna58@gmail.com>
2023-04-12 14:56:08 -0700

    JavaScript_WarmUp

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const firstLine = "C is fun";
+console.log(firstLine);
+
+let secondLine = "Python is cool";
+console.log(secondLine);
+
+const thirdLine ="JavaScript is amazing";
+console.log(thirdLine);
