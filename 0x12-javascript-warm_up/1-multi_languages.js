9812b4cd2998dad644c09ac563e9202e8881b80c
Dike-Code <dikeclintonobinna58@gmail.com>
2023-04-13 06:31:10 -0700

    Js_warmup

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const firstLine = "C is fun";
+const firstLine = 'C is fun';
 console.log(firstLine);
 
-let secondLine = "Python is cool";
+const secondLine = 'Python is cool';
 console.log(secondLine);
 
-const thirdLine ="JavaScript is amazing";
+const thirdLine = 'JavaScript is amazing';
 console.log(thirdLine);
