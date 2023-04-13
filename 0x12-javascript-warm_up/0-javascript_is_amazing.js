859f8ae404e63a27208495993d794c299b8533b8
Dike-Code <dikeclintonobinna58@gmail.com>
2023-04-13 06:22:50 -0700

    js_warmup

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-//Print  String "JavaScript is amazing"
-const myVar = "JavaScript is amazing";
+// Print  String 'JavaScript is amazing'
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
